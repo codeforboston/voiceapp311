e7cbc584a03d6afdb42f1349d573f324909ab083
Avery, Brian <brian.avery@one.verizon.com>
2017-09-23 15:59:47 -0400

    Initial commit of common service

---
+++ service/app.py
@@ -0,0 +1,35 @@
+import requests
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+#intents
+#
+
+
+@app.route('/')
+def index():
+    return "Hello World!";
+
+@app.route('/app/v1/request')
+def request():
+    url = "https://data.cityofboston.gov/resource/cp2t-tvxx.json"
+
+    headers = {
+     
+        'cache-control': "no-cache"
+        }
+    response = requests.get(url, headers=headers)
+    json_data = response.json()
+
+    parsed_data = []
+    for entry in json_data[:]:
+        parsed_data.append({
+            'recycling':entry["recycling"],
+            'district':entry["publicworksdistrict"],
+            'trash': entry["trash"],
+        })
+    return jsonify({'foo':json_data})
+
+if __name__ == '__main__':
+    app.run(debug=True)
