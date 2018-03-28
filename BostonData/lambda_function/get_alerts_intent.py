4ea346fa74a0500c0f7291bb9fc880b2f81a9ecc
Wes Drew <wesdrew@gmail.com>
2018-03-27 21:14:14 -0400

    added constants for html tags

--- BostonData/lambda_function/get_alerts_intent.py
+++ BostonData/lambda_function/get_alerts_intent.py
@@ -37,7 +37,17 @@ class Services(Enum):
     PUBLIC_TRANSIT = 'Public Transit'
     SCHOOLS = 'Schools'
     ALERT_HEADER = 'Alert header'
-    
+
+# constants for scraping boston.gov
+BOSTON_GOV = "https://www.boston.gov"
+SERVICE_NAMES = "cds-t t--upper t--sans m-b300"
+SEVICE_INFO = "cds-d t--subinfo"
+HEADER_1 = "t--upper t--sans lh--000 t--cb"
+HEADER_2 = "t--upper t--sans lh--000 t--cb"
+HEADER_3 = "str str--r m-v300"
+HEADER_4 = "t--sans t--cb lh--000 m-b500"
+ALT_HEADER_1 = "t--sans t--ob lh--000 m-b500"
+
 
 def get_alerts_intent(intent, session):
     """
@@ -92,23 +102,23 @@ def prune_normal_responses(service_alerts):
     return service_alerts
 
 def get_alerts():
-    url = urllib.request.urlopen("https://www.boston.gov") # get page
+    url = urllib.request.urlopen(BOSTON_GOV) # get page
     soup = BeautifulSoup(url, "html.parser")               # feed into BS
     url.close()
     # parse, sanitize returned strings, place in dictionary
-    services = [s.text.strip() for s in soup.find_all(class_ = "cds-t t--upper t--sans m-b300")]
-    service_info = [s_info.text.strip().replace(u'\xA0', u' ') for s_info in soup.find_all(class_ = "cds-d t--subinfo")]
+    services = [s.text.strip() for s in soup.find_all(class_ = SERVICE_NAMES)]
+    service_info = [s_info.text.strip().replace(u'\xA0', u' ') for s_info in soup.find_all(class_ = SERVICE_INFO)]
     alerts = {}
     for i in range(len(services)):
         alerts[services[i]] = service_info[i]
     # get alert header, if any (this is something like "Winter Storm warning")
     header = ""
-    if soup.find(class_ = "t--upper t--sans lh--000 t--cb") != None: # no guarantee that this tag will always be the header
-        header += soup.find(class_ = "t--upper t--sans lh--000 t--cb").text + '. '
-        header += soup.find(class_ = "str str--r m-v300").text + '. ' 
-        header += soup.find(class_ = "t--sans t--cb lh--000 m-b500").text + ' '
+    if soup.find(class_ = HEADER_1) != None: # no guarantee that this tag will always be the header
+        header += soup.find(class_ = HEADER_2).text + '. '
+        header += soup.find(class_ = HEADER_3).text + '. ' 
+        header += soup.find(class_ = HEADER_4).text + ' '
 
-    if soup.find(class_ = "t--sans t--ob lh--000 m-b500") != None: # another possible header tag, again this might change 
-        header += soup.find(class_ = "t--sans t--ob lh--000 m-b500").text
+    if soup.find(class_ = ALT_HEADER_1) != None: # another possible header tag, again this might change 
+        header += soup.find(class_ = ALT_HEADER_1).text
     alerts[Services.ALERT_HEADER.value] = header.rstrip()
     return alerts
