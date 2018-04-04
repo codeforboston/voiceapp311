be17424a46ff3d13bc8f7ba7e7f40793ad5d4058
Jason Curran <curran.jm@gmail.com>
2018-04-03 21:02:37 -0400

    fix wording in intent constants file comment

--- mycity/mycity/intents/intent_constants.py
+++ mycity/mycity/intents/intent_constants.py
@@ -1,4 +1,4 @@
-"""Constants used across Alexa intents"""
+"""Constants used across intents"""
 
 # The key used for the current address in session attributes
 CURRENT_ADDRESS_KEY = "currentAddress"
