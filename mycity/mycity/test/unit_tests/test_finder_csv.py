5e0059ab08491d0514b8acd1e6831db53e571278
Wes Drew <wesdrew@gmail.com>
2018-05-14 17:24:50 -0400

    TestCase for FinderCSV has a couple tests but not much else. Needs to be expanded.

--- mycity/mycity/test/unit_tests/test_finder_csv.py
+++ mycity/mycity/test/unit_tests/test_finder_csv.py
@@ -1,29 +1,45 @@
 import unittest.mock as mock
 
+import mycity.utilities.google_maps_utils as g_maps_utils
 import mycity.test.test_constants as test_constants
 import mycity.test.unit_tests.base as base
-import mycity.utilities.finder_csv as finder_csv
+from mycity.utilities.finder.FinderCSV import FinderCSV
 
 
 class FinderCSVTestCase(base.BaseTestCase):
 
+
+    def setUp(self):
+        def test_prep_func(keys):
+            if keys["Address"] == "123 Fake St Boston, MA":
+                keys["Address"] = "123 Real St Boston, MA"
+        super().setUp()
+        fake_url = "www.fake.com"
+        address_key = "Address"
+        output_speech = "Trying to get {name}, {" + address_key + "}, " \
+            + "{" + g_maps_utils.DRIVING_DISTANCE_TEXT_KEY + "}."
+        self.request.session_attributes['currentAddress'] = '1000 Dorchester Ave'
+        self.finder = FinderCSV(self.request, fake_url, address_key,
+                                output_speech, test_prep_func)
+
+    def tearDown(self):
+        self.finder = None
+        super().tearDown()
+
+
     def test_get_output_speech_with_success(self):
-        pass
+        distance = g_maps_utils.DRIVING_DISTANCE_TEXT_KEY
+        self.finder.set_output_speech({'Address' : '123 Fake St',
+                                       'name' : 'The Place',
+                                       distance : '100 miles away'})
+        self.assertEqual("Trying to get The Place, 123 Fake St, 100 miles away.",
+                         self.finder.output_speech)
+
 
     def test_get_output_speech_after_error(self):
-        pass
+        self.finder.set_output_speech({'MissingKeys' : 'Address, name, distance'})
+        self.assertEqual(self.finder.ERROR_MESSAGE, self.finder.output_speech)
 
-    def test_no_explicit_filter_returning_all_records(self):
-        pass
-
-    def test_filter_returns_only_one_record(self):
-        pass
-
-    def test_fails_cleanly_if_file_not_found(self):
-        pass
-
-    def test_set_output_speech(self):
-        pass
 
 
 
--- mycity/run_tests.py
+++ mycity/run_tests.py
@@ -4,6 +4,7 @@ import mycity.test.integration_tests.test_snow_emergency_parking as snow_parking
 import mycity.test.integration_tests.test_trash_intent as trash_intent
 import mycity.test.integration_tests.test_unhandled_intent as unhandled_intent
 import mycity.test.integration_tests.test_get_alerts as get_alerts
+import mycity.test.unit_tests.test_finder_csv as finder_csv
 import mycity.test.unit_tests.test_mycity_controller as my_controller
 import mycity.test.unit_tests.test_address_utils as address_utils
 import mycity.test.unit_tests.test_csv_utils as csv_utils
@@ -14,6 +15,7 @@ TEST_CASES = [  my_controller.MyCityControllerUnitTestCase,
                 address_utils.AddressUtilitiesTestCase,
                 csv_utils.CSVUtilitiesTestCase,
                 gis_utils.GISUtilitiesTestCase,
+                finder_csv.FinderCSVTestCase,
                 get_alerts.GetAlertsTestCase,
                 trash_intent.TrashDayTestCase,
                 unhandled_intent.UnhandledIntentTestCase,
