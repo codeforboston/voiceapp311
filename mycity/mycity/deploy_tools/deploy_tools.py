f7fa7976c27b855225f4ae880adcbfdbb5a7d57d
Jason Curran <curran.jm@gmail.com>
2018-06-14 20:15:06 -0400

    Deploy_tools.py cleanup (#97)
    Addresses the following:
      - Issue 92
        The cleanup portion of the script is unable to delete the temp dir
        on Windows.
          - This adds a check for a read-only failure and modifies
            the permissions on the folder, then retries the delete.
      - Issue 93
        The pip version check was comparing versions strings instead of
        version numbers.
          - The pip version check is unnecessary; it was there to deal with
            the deprecated pip.main function, but we don't need to use that
            function. Instead, we can compose the command and execute it in
            the containing environment via subprocess.run. This should work
            regardless of pip version.

--- mycity/mycity/deploy_tools/deploy_tools.py
+++ mycity/mycity/deploy_tools/deploy_tools.py
@@ -1,38 +1,37 @@
-'''
-Tools to package and deploy the lambda function for Boston Data app.
-'''
+"""
+Tools to package and deploy the lambda function for the mycity voice app.
+"""
 
 from __future__ import print_function
+from subprocess import run
 import argparse
 import os
-import pip
 import shutil
-import subprocess
 import zipfile
+import stat
+import errno
 
-# TODO: This needs to be updated to give user some idea of how to run it and where to run it from
+# path constants
 PROJECT_ROOT = os.path.join(os.getcwd(), os.pardir, os.pardir)
 TEMP_DIR_PATH = os.path.join(PROJECT_ROOT, 'temp')
 LAMBDA_REL_PATH = 'platforms/amazon/lambda/custom/lambda_function.py'
 LAMBDA_FUNCTION_PATH = os.path.join(PROJECT_ROOT, LAMBDA_REL_PATH)
 MYCITY_PATH = os.path.join(PROJECT_ROOT, 'mycity')
 
-INSTALL_REQUIREMENTS_SCRIPT = os.path.join(os.getcwd(), 'install_requirements.sh')
+ZIP_FILE_NAME = "lambda_function.zip"
 
 
 def zip_lambda_function_directory(zip_target_dir):
     """
     Generates a .zip file containing the contents of the temporary directory
     where the project files have been copied. Note that this .zip file
-    contains the files with no intermediate directory.
+    must contain the files with no intermediate directory.
 
     :param zip_target_dir: destination directory for zip file being created
     :return: none
     """
-    # TODO: revise this to avoid changing directory
-    os.chdir(zip_target_dir)
-    zip_file_name = "lambda_function.zip"
-    zip_file = zipfile.ZipFile(zip_file_name, 'w')
+    zip_file = zipfile.ZipFile(os.path.join(zip_target_dir, ZIP_FILE_NAME), 'w')
+    original_directory = os.getcwd()
     os.chdir(TEMP_DIR_PATH)
     print('Compressing ', end='')
     for root, dirs, files in os.walk('.'):
@@ -41,59 +40,42 @@ def zip_lambda_function_directory(zip_target_dir):
             print('.', end='')
     print('DONE')
     zip_file.close()
+    os.chdir(original_directory)
 
 
 def install_pip_dependencies(requirements_path, requirements_path_no_deps):
     """
-    Wraps both _install_pip_dependencies. pip version is checked in this
-    function so correct install function is called.
-
-    if pip version >= '10.0.0', we should install from script
-    else, install using pip.main
+    Installs all the dependencies for the project's entry point to a
+    temporary directory the .zip file is later created from.
 
     :param requirements_path: path to textfile containing required libraries
     :param requirements_path_no_deps: path to textfile containing required
         libraries (whose dependencies won't be downloaded)
     :return: none
     """
-    version = pip.__version__
-    if version >= '10.0.0':
-        _install_pip_dependencies_from_script()
-    else:
-        _install_pip_dependencies(requirements_path, requirements_path_no_deps)
+    install_args = [
+        "pip",
+        "install",
+        "-r",
+        requirements_path,
+        "-t",
+        TEMP_DIR_PATH
+    ]
 
+    install_args_no_deps = [
+        "pip",
+        "install",
+        "--no-deps",
+        "-r",
+        requirements_path_no_deps,
+        "-t",
+        TEMP_DIR_PATH
+    ]
 
-def _install_pip_dependencies(requirements_path, requirements_path_no_deps):
-    """
-    Uses requirements.txt to install all external libraries used by the project
-    in the temporary directory the .zip file is created from.
-
-    :param requirements_path: path to textfile containing required libraries
-    :param requirements_path_no_deps: path to textfile containing required
-        libraries (whose dependencies won't be downloaded)
-    :return: none
-    """
     print('Installing dependencies ... ', end='')
-    install_args = "pip install -r " + requirements_path + " -t " + TEMP_DIR_PATH
-    pip.main(install_args)
+    run(install_args)
     print('Installing dependencies from requirements_no_deps.txt ...', end='')
-    install_args_no_deps = ("pip install --no-deps -r ",
-                            requirements_paths_no_deps,
-                            " -t ", 
-                            TEMP_DIR_PATH)
-    pip.main(install_args_no_deps)
-    print('DONE')
-
-
-def _install_pip_dependencies_from_script():
-    """
-    pip.main is deprecated in the latest version of pip. If pip is version 10.0.0 
-    or later, we can install our dependencies using a shell script
-
-    :return: none
-    """
-    print('Installing dependencies ... ', end='')
-    subprocess.call(INSTALL_REQUIREMENTS_SCRIPT)
+    run(install_args_no_deps)
     print('DONE')
 
 
@@ -106,23 +88,60 @@ def package_lambda_function():
     :return: none
     """
     print('Creating temporary build directory ... ', end='')
-    # create the temporary directory for the zip file's contents
+    # remove/create the temporary directory for the zip file's contents
+    if os.path.exists(TEMP_DIR_PATH):
+        shutil.rmtree(
+            TEMP_DIR_PATH,
+            ignore_errors=False,
+            onerror=handle_remove_readonly
+        )
     os.mkdir(TEMP_DIR_PATH)
-    # copy the lambda file, mycity files, and requirements.txt to temp directory
+
+    # copy lambda file and mycity directory to temp directory
     shutil.copy(LAMBDA_FUNCTION_PATH, TEMP_DIR_PATH)
     shutil.copytree(MYCITY_PATH, os.path.join(TEMP_DIR_PATH, 'mycity'))
-    requirements_path = os.path.join(os.getcwd(), 'requirements.txt')
-    requirements_path_no_deps = os.path.join(os.getcwd(), 'requirements_no_deps.txt')
-    shutil.copy(requirements_path, TEMP_DIR_PATH)
+
     print('DONE')
+
     # install dependencies
-    install_pip_dependencies(requirements_path, requirements_path_no_deps)
+    install_pip_dependencies(
+        os.path.join(os.getcwd(), 'requirements.txt'),
+        os.path.join(os.getcwd(), 'requirements_no_deps.txt')
+    )
+
     # build zip file in project root
     zip_lambda_function_directory(PROJECT_ROOT)
+
     # delete temp directory
     print('Cleaning up ... ', end='')
-    shutil.rmtree(TEMP_DIR_PATH)
+    shutil.rmtree(
+        TEMP_DIR_PATH,
+        ignore_errors=False,
+        onerror=handle_remove_readonly
+    )
     print('DONE')
+
+
+def handle_remove_readonly(func, path, execinfo):
+    """
+    Passed as the onerror parameter when calling shutil.rmtree.
+    See:
+    https://stackoverflow.com/a/1214935/2554154
+    Handles the case where rmtree fails in Windows due to access problems.
+
+    :param func:
+    :param path:
+    :param execinfo:
+    :return: none
+    """
+    excvalue = execinfo[1]
+    if func in (os.rmdir, os.remove) and excvalue.errno == errno.EACCES:
+        # if we're failing to remove files because they are readonly,
+        # update permissions
+        os.chmod(path, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)  # 0777
+        func(path)
+    else:
+        raise Exception("Failed to delete temp folder.")
 
 
 def main():
@@ -132,12 +151,18 @@ def main():
 
     :return: none
     """
-    parser = argparse.ArgumentParser(description="Tools to " +
-        "package and deploy the lambda function for Boston Data app")
+    parser = argparse.ArgumentParser(
+        description="Tools to package and deploy the lambda function for the " +
+                    "MyCity app."
+    )
 
-    parser.add_argument('-p', '--package', help="Creates a zip file " +
-        "that can be uploaded as an Amazon lambda function",
-        action='store_true')
+    parser.add_argument(
+        '-p',
+        '--package',
+        help="Creates a zip file that can be uploaded as an Amazon lambda " +
+             "function",
+        action='store_true'
+    )
 
     args = parser.parse_args()
 
