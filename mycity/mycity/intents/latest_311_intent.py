d9a14a0d71b6f68306fc72ac95ae798b951b7747
jmartini <martini.jamie@gmail.com>
2018-11-11 17:08:30 -0500

    Add read latest 311 45 (#166)
    * Add intent to read off latest 311 reports
    
    * Change import path
    
    * Revert "Change import path"
    
    This reverts commit 9cff9f2b33c768a2f632f784a2744c87c7d42801.
    
    * Fix import paths and make helper function to get number of reports requested
    
    * Use resource constants
    
    * Add missing parameter docstring

---
+++ mycity/mycity/intents/latest_311_intent.py
@@ -0,0 +1,108 @@
+import requests
+from mycity.mycity_response_data_model import MyCityResponseDataModel
+from mycity.intents.custom_errors import BadAPIResponse
+from mycity.intents.speech_constants.latest_311_constants import *
+
+DEFAULT_NUMBER_OF_REPORTS = 3
+
+BOSTON_311_URL = "https://data.boston.gov/api/3/action/datastore_search"
+BOSTON_RESOURCE_ID = "https://data.boston.gov/api/3/action/datastore_search"
+
+
+def get_311_requests(mycity_request):
+    """
+    Generates response object for a 311 request inquiry.
+
+    :param mycity_request: MyCityRequestDataModel object
+    :return: MyCityResponseDataModel object
+    """
+    mycity_response = MyCityResponseDataModel()
+
+    try:
+        number_reports = number_of_reports(mycity_request)
+
+        request_entries = \
+            get_311_requests_from_server(number_reports)
+        mycity_response.output_speech = \
+            REQUEST_311_INTRO_SCRIPT.format(number_reports)
+        for request_entry in request_entries:
+            mycity_response.output_speech += \
+                build_speech_from_311_report(request_entry)
+
+        mycity_response.card_title = REQUEST_311_CARD_TITLE
+
+    except BadAPIResponse:
+        mycity_response.output_speech = BAD_API_RESPONSE
+
+    return mycity_response
+
+
+def number_of_reports(mycity_request):
+    """
+    Returns number of reports from the request if available or a default value
+    :param mycity_request: MyCityRequestDataModel object
+    :return: Number of 311 requests to return from this intent
+    """
+    if REQUEST_311_NUMBER_REPORTS_SLOT_NAME in \
+            mycity_request.intent_variables and \
+            "value" in mycity_request.intent_variables[
+                REQUEST_311_NUMBER_REPORTS_SLOT_NAME]:
+        return mycity_request.intent_variables[
+                REQUEST_311_NUMBER_REPORTS_SLOT_NAME]["value"]
+
+    return DEFAULT_NUMBER_OF_REPORTS
+
+
+def get_311_requests_from_server(number_entries):
+    """
+    Returns the 311 data of the latest number_entries of requests to Boston's
+    311
+
+    :param number_entries: Number of entries to return
+    :return: JSON object containing array of 311 requests.
+    """
+
+    response_json = get_raw_311_reports_json(number_entries)
+    return response_json["result"]["records"]
+
+
+def get_raw_311_reports_json(number_entries):
+    """
+    Returns the JSON object from the 311 API
+
+    :param number_entries: Number of entries to return
+    :return: JSON object containing 311 data
+    """
+    data_url = BOSTON_311_URL
+    parameters = {
+        "resource_id": BOSTON_RESOURCE_ID,
+        "limit": number_entries
+    }
+
+    response = requests.get(data_url, parameters)
+    if response.status_code != requests.codes.ok:
+        raise BadAPIResponse
+
+    if "result" not in response.json() or \
+            "records" not in response.json()["result"]:
+        # Unexpected JSON format. Missing expected keys
+        raise BadAPIResponse
+
+    return response.json()
+
+
+def build_speech_from_311_report(report):
+    """
+    Builds a speech string from a given 311 report
+    :param report: JSON object of a single 311 report
+    :return: Speech string representing this report
+    """
+
+    try:
+        subject = report["SUBJECT"]
+        report_type = report["TYPE"]
+        location = report["LOCATION_STREET_NAME"]
+    except KeyError:
+        raise BadAPIResponse
+
+    return REQUEST_311_REPORT_SCRIPT.format(location, subject, report_type)
---
+++ mycity/mycity/test/integration_tests/test_latest_311_intent.py
@@ -0,0 +1,73 @@
+import unittest.mock as mock
+import mycity.intents.speech_constants.latest_311_constants \
+    as intent_constants
+import mycity.test.integration_tests.intent_test_mixins as mix_ins
+import mycity.test.integration_tests.intent_base_case as base_case
+from mycity.test.test_data.latest_311_fake_data import *
+
+
+class Latest311TestCase(mix_ins.RepromptTextTestMixIn,
+                        mix_ins.CardTitleTestMixIn,
+                        mix_ins.CorrectSpeechOutputTestMixIn,
+                        base_case.IntentBaseCase):
+
+    intent_to_test = "LatestThreeOneOne"
+    expected_title = intent_constants.REQUEST_311_CARD_TITLE
+    returns_reprompt_text = False
+    expected_card_title = intent_constants.REQUEST_311_CARD_TITLE
+
+    def setUp(self):
+        """
+        Set up unit tests by mocking calls to 311
+        """
+        super().setUp()
+        self.mock_311_service = mock.patch(
+            'mycity.intents.latest_311_intent.get_raw_311_reports_json',
+            return_value=FAKE_JSON_RESPONSE_3).start()
+
+    def tearDown(self):
+        """
+        Stop mocked objects
+        """
+        super().tearDown()
+        self.mock_311_service.stop()
+
+    def test_slot_determines_number_of_reports(self):
+        num_reports = 1
+        self.request.intent_variables = \
+            {
+                intent_constants.REQUEST_311_NUMBER_REPORTS_SLOT_NAME:
+                    {"value": num_reports}
+            }
+        self.mock_311_service.return_value = FAKE_JSON_RESPONSE_1
+        response = self.controller.on_intent(self.request)
+        self.mock_311_service.assert_called_once_with(num_reports)
+        self.assertTrue(FAKE_LOCATION_1 in response.output_speech)
+        self.assertTrue(FAKE_TYPE_1 in response.output_speech)
+        self.assertTrue(FAKE_SUBJECT_1 in response.output_speech)
+
+    def test_speech_can_contain_multiple_reports(self):
+        num_reports = 3
+        self.request.intent_variables = \
+            {
+                intent_constants.REQUEST_311_NUMBER_REPORTS_SLOT_NAME:
+                    {"value": num_reports}
+            }
+        self.mock_311_service.return_value = FAKE_JSON_RESPONSE_3
+        response = self.controller.on_intent(self.request)
+        self.mock_311_service.assert_called_once_with(num_reports)
+
+        # Data group 1
+        self.assertTrue(FAKE_LOCATION_1 in response.output_speech)
+        self.assertTrue(FAKE_TYPE_1 in response.output_speech)
+        self.assertTrue(FAKE_SUBJECT_1 in response.output_speech)
+
+        # Data group 1
+        self.assertTrue(FAKE_LOCATION_2 in response.output_speech)
+        self.assertTrue(FAKE_TYPE_2 in response.output_speech)
+        self.assertTrue(FAKE_SUBJECT_2 in response.output_speech)
+
+        # Data group 1
+        self.assertTrue(FAKE_LOCATION_3 in response.output_speech)
+        self.assertTrue(FAKE_TYPE_3 in response.output_speech)
+        self.assertTrue(FAKE_SUBJECT_3 in response.output_speech)
