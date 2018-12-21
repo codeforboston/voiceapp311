c4cadc19e376a38f8a18b60263e8fcc5668440c4
Joshua D <joshdecosta@gmail.com>
2018-12-21 15:38:59 -0500

    fixed dialog directive for VotingIntent

--- mycity/mycity/intents/voting_intent.py
+++ mycity/mycity/intents/voting_intent.py
@@ -3,10 +3,36 @@ Functions for voting information including polling location information
 """
 
 from . import intent_constants
+from streetaddress import StreetAddressParser
 from mycity.mycity_response_data_model import MyCityResponseDataModel
 import mycity.utilities.arcgis_utils as arcgis_utils
 import requests
 import logging
 
 logger = logging.getLogger(__name__)
+CARD_TITLE = "Polling Location"
 
+
+def get_polling_location(mycity_request):
+    """
+    Generates response object for a polling location inquiry
+
+    :param mycity_request: MyCityRequestDataModel object
+    :return: MyCityResponseDataModel object
+    """
+
+    logger.debug('MyCityRequestDataModel received:' + mycity_request.get_logger_string())
+    mycity_response = MyCityResponseDataModel()
+    
+    current_address = \
+        mycity_request.session_attributes[intent_constants.CURRENT_ADDRESS_KEY]
+    current_address += ", Boston, MA"
+    candidates = arcgis_utils.geocode_address_candidates(current_address)
+    top_candidates = arcgis_utils.select_top_address_candidate(candidates)
+    ward_precinct = arcgis_utils.get_ward_precinct_info(top_candidates)
+    poll_location = arcgis_utils.get_polling_location(ward_precinct)
+    poll_speech = "Your polling location is {}, {}".format(poll_location["Location Name"], poll_location["Location Address"])
+    mycity_response.output_speech = poll_speech
+    mycity_response.reprompt_text = None
+    mycity_response.reprompt_text = CARD_TITLE
+    return mycity_response
--- mycity/mycity/mycity_controller.py
+++ mycity/mycity/mycity_controller.py
@@ -12,6 +12,7 @@ from .intents.trash_intent import get_trash_day_info
 from .intents.unhandled_intent import unhandled_intent
 from .intents.get_alerts_intent import get_alerts_intent
 from .intents.snow_parking_intent import get_snow_emergency_parking_intent
+from .intents.voting_intent import get_polling_location
 from .intents.feedback_intent import submit_feedback
 from .intents import intent_constants
 import logging
@@ -124,6 +125,11 @@ def on_intent(mycity_request):
             else get_snow_emergency_parking_intent(mycity_request)
     elif mycity_request.intent_name == "GetAlertsIntent":
         return get_alerts_intent(mycity_request)
+    elif mycity_request.intent_name == "VotingIntent":
+        return request_user_address_response(mycity_request) \
+            if intent_constants.CURRENT_ADDRESS_KEY \
+            not in mycity_request.session_attributes \
+            else get_polling_location(mycity_request)
     elif mycity_request.intent_name == "AMAZON.HelpIntent":
         return get_help_response(mycity_request)
     elif mycity_request.intent_name == "AMAZON.StopIntent" or \
