6db5e6df4b1a668c60a2f3bf114e4cc2beb2c305
Eric Shaw <shaweric01@gmail.com>
2019-01-29 13:19:33 -0500

    test runner fix

--- mycity/run_tests.sh
+++ mycity/run_tests.sh
@@ -1,1 +1,8 @@
+# Dummy variables must be set in order to avoid KeyError being raised during testing
+
+# see issue #187 to determine if GOOGLE_MAPS_API can be removed along with this comment.
+export GOOGLE_MAPS_API_KEY=FAKEFAKEFAKE
+
+export SLACK_WEBHOOKS_URL=FAKEFAKEFAKE
+
 python -m unittest discover -v -s mycity/test
