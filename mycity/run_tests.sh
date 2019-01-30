712d82c2abd897f1d8ed6d2e4f74818493c085eb
eric-s-s <shaweric01@gmail.com>
2019-01-29 20:18:58 -0500

    test runner fix (#201)
    * test runner fix
    
    * updated to check if exist

--- mycity/run_tests.sh
+++ mycity/run_tests.sh
@@ -1,1 +1,12 @@
+
+# see issue #187 to determine if GOOGLE_MAPS_API can be removed along with this comment.
+if [ -z "${GOOGLE_MAPS_API_KEY}" ];then
+    export GOOGLE_MAPS_API_KEY=FAKEFAKEFAKE
+fi
+
+if [ -z "${SLACK_WEBHOOKS_URL}" ]; then
+    export SLACK_WEBHOOKS_URL=FAKEFAKEFAKE
+fi
+
 python -m unittest discover -v -s mycity/test
+
