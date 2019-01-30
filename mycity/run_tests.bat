a7d89247ff435b6a7b70f052236db30beb5a15ea
ethanstrominger <ethanstrominger2@gmail.com>
2019-01-29 19:53:43 -0500

    Check if variable defined before setting to a dummy value

--- mycity/run_tests.bat
+++ mycity/run_tests.bat
@@ -4,6 +4,6 @@ REM Note:
 REM   See issue #187 for removing GOOGLE_MAPS_API_KEY can be removed along with this comment
 REM   SLACK_WEBHOOKS_URL is required for AWS Lamba and is set up correctly
 
-set SLACK_WEBHOOKS_URL=FAKEFAKEFAKE
-set GOOGLE_MAPS_API_KEY=FAKEFAKEFAKE
+if not defined SLACK_WEBHOOKS_URL set SLACK_WEBHOOKS_URL=FAKEFAKEFAKE
+if not defined GOOGLE_MAPS_API_KEY set GOOGLE_MAPS_API_KEY=FAKEFAKEFAKE
 python -m unittest discover -v -s mycity\test
