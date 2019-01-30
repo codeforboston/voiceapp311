6cbdcf1edb9ad70283db3f1dc59375028af96052
Eric Shaw <shaweric01@gmail.com>
2019-01-29 19:54:31 -0500

    updated to check if exist

--- mycity/run_tests.sh
+++ mycity/run_tests.sh
@@ -1,8 +1,12 @@
-# Dummy variables must be set in order to avoid KeyError being raised during testing
 
 # see issue #187 to determine if GOOGLE_MAPS_API can be removed along with this comment.
-export GOOGLE_MAPS_API_KEY=FAKEFAKEFAKE
+if [ -z "${GOOGLE_MAPS_API_KEY}" ];then
+    export GOOGLE_MAPS_API_KEY=FAKEFAKEFAKE
+fi
 
-export SLACK_WEBHOOKS_URL=FAKEFAKEFAKE
+if [ -z "${SLACK_WEBHOOKS_URL}" ]; then
+    export SLACK_WEBHOOKS_URL=FAKEFAKEFAKE
+fi
 
 python -m unittest discover -v -s mycity/test
+
