65e6d742cb7e8e2ffab33741f45a2466dbfe4b2a
Joshua Decosta <joshdecosta@gmail.com>
2019-02-05 20:12:40 -0500

    ends sessions for voting_intent

--- mycity/mycity/intents/voting_intent.py
+++ mycity/mycity/intents/voting_intent.py
@@ -35,4 +35,5 @@ def get_polling_location(mycity_request):
     mycity_response.output_speech = output_speech
     mycity_response.reprompt_text = None
     mycity_response.reprompt_text = CARD_TITLE
+    mycity_response.should_end_session = False
     return mycity_response
