59ac4c32d56c2d4bac9a3eeb72384da4c744f595
Joshua Decosta <joshdecosta@gmail.com>
2019-02-19 19:42:29 -0500

    changed arcgistestcase to voteutilstestcase'

--- mycity/mycity/test/unit_tests/test_arcgis_utils.py
+++ mycity/mycity/test/unit_tests/test_arcgis_utils.py
@@ -1,12 +1,12 @@
 import mycity.test.unit_tests.base as base
 import unittest.mock as mock
 import mycity.test.test_constants as test_constants
-import mycity.utilities.arcgis_utils as arcgis_utils
+import mycity.utilities.voting_utils as vote_utils
 import mycity.mycity_controller as my_con
 import mycity.intents.intent_constants as intent_constants
 
 
-class ArcgisUtilitiesTestCase(base.BaseTestCase):
+class VotingUtilitiesTestCase(base.BaseTestCase):
     
     @mock.patch('requests.request')
     def test_get_ward_precinct_info(self, mock_get):
@@ -14,7 +14,7 @@ class ArcgisUtilitiesTestCase(base.BaseTestCase):
             json_data=test_constants.MOCK_WARD_PRECINCT_RESP)
         mock_get.return_value = mock_resp
         expected_output_text = test_constants.WARD_PRECINCT
-        result = arcgis_utils.get_ward_precinct_info(test_constants.COORDS)
+        result = vote_utils.get_ward_precinct_info(test_constants.COORDS)
         self.assertEquals(expected_output_text, result)
 
     @mock.patch('requests.request')
@@ -23,5 +23,5 @@ class ArcgisUtilitiesTestCase(base.BaseTestCase):
             json_data=test_constants.MOCK_POLL_RESP)
         mock_get.return_value = mock_resp
         expected_output_text = test_constants.POLL_DATA
-        result = arcgis_utils.get_polling_location(test_constants.WARD_PRECINCT)
+        result = vote_utils.get_polling_location(test_constants.WARD_PRECINCT)
         self.assertEquals(expected_output_text, result)
