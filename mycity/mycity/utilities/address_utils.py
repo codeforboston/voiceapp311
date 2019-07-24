7fe46d3422b48359295538107e148979035c45a8
Hameed Gifford <giff.h92@gmail.com>
2019-07-23 20:03:39 -0400

    Exclude potential None values from string join

--- mycity/mycity/utilities/address_utils.py
+++ mycity/mycity/utilities/address_utils.py
@@ -14,7 +14,7 @@ def build_origin_address(req):
     """
     Builds an address from an Alexa session. Assumes city is Boston if not
     specified
-    
+
     :param req: MyCityRequestDataModel object
     :return: String containing full address
     """
@@ -23,14 +23,15 @@ def build_origin_address(req):
     current_address = \
         req.session_attributes[intent_constants.CURRENT_ADDRESS_KEY]
     parsed_address = address_parser.parse(current_address)
-    origin_address = " ".join([parsed_address["house"],
-                               parsed_address["street_full"]])
+    origin_address = " ".join(filter(None, [parsed_address["house"],
+                                            parsed_address["street_full"]]))
     if parsed_address["other"]:
         origin_address += " {}".format(parsed_address["other"])
     else:
         origin_address += " Boston MA"
 
     return origin_address
+
 
 def is_address_valid(address):
     """
