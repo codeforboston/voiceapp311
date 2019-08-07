6b4c6383b9646538cc21832bb0c739c706440656
Jaumb <Jeffrey.Amari001@umb.edu>
2019-08-06 20:46:31 -0400

    Move common functions into address utils

--- mycity/mycity/utilities/address_utils.py
+++ mycity/mycity/utilities/address_utils.py
@@ -3,10 +3,12 @@ Utility function for building an address string from a mycity request
 
 """
 
-from streetaddress import StreetAddressParser
+import mycity.utilities.gis_utils as gis_utils
 import mycity.intents.intent_constants as intent_constants
+import mycity.utilities.location_services_utils as location_services_utils
 import logging
 
+from streetaddress import StreetAddressParser
 from mycity.intents.custom_errors import InvalidAddressError
 
 logger = logging.getLogger(__name__)
@@ -53,3 +55,37 @@ def is_address_valid(address):
         key in address
         and address[key] is not None
         for key in ("house", "street_full"))
+
+
+def get_address_coordinates_from_session(mycity_request) -> dict:
+    """
+    Gets coordinates of the provided address from the session attributes.
+    Returns None if no address is available.
+    :param mycity_request: MyCityRequestDataModel for the current request
+    :return dict: Dictionary containing coordinates of the address
+    """
+    user_address = None
+    if intent_constants.CURRENT_ADDRESS_KEY in mycity_request.session_attributes:
+        current_address = mycity_request.session_attributes[intent_constants.CURRENT_ADDRESS_KEY]
+        parsed_address = StreetAddressParser().parse(current_address)
+        address = " ".join([
+            parsed_address["house"],
+            parsed_address["street_name"],
+            parsed_address["street_type"]])
+        user_address = gis_utils.geocode_address(address)
+
+    return user_address
+
+
+def get_address_coordinates_from_geolocation(mycity_request) -> dict:
+    """
+    Gets coordinates of the device location. Returns None if not provided
+    or not accessible.
+    :param mycity_request: MyCityRequestDataModel for the current request
+    :return dict: Dictionary containing coordinates of the device
+    """
+    user_address = None
+    if mycity_request.device_has_geolocation:
+        if mycity_request.geolocation_permission:
+            user_address = location_services_utils.convert_mycity_coordinates_to_arcgis(mycity_request)
+    return user_address
