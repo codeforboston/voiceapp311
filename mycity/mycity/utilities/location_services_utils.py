db5fb6f818e07845693809f5d1fbc0cf29c40c81
Joshua Decosta <joshdecosta@gmail.com>
2019-08-06 20:05:20 -0400

    adds device location permission response function

--- mycity/mycity/utilities/location_services_utils.py
+++ mycity/mycity/utilities/location_services_utils.py
@@ -2,9 +2,14 @@
 
 import mycity.mycity_response_data_model as mycity_response_data_model
 
-GENERIC_GEOLOCATION_PRERMISSON_SPEECH = """
+GENERIC_GEOLOCATION_PERMISSON_SPEECH = """
     Boston Info would like to use your location. 
     To turn on location sharing, please go to your Alexa app and follow the instructions."""
+
+GENERIC_DEVICE_PERMISSON_SPEECH = """
+    Boston Info would like to use your device's address. 
+    To turn on location sharing, please go to your Alexa app and follow the instructions."""
+
 
 def request_geolocation_permission_response():
     """
@@ -15,12 +20,26 @@ def request_geolocation_permission_response():
         to request geolocation access
     """
     response = mycity_response_data_model.MyCityResponseDataModel()
-    response.output_speech = GENERIC_GEOLOCATION_PRERMISSON_SPEECH
+    response.output_speech = GENERIC_GEOLOCATION_PERMISSON_SPEECH
     response.card_type = "AskForPermissionsConsent"
     response.card_permissions = ["alexa::devices:all:geolocation:read"]
     response.should_end_session = True
     return response
 
+def request_device_address_permission_response():
+    """
+    Builds a response object for requesting geolocation permissions. The
+    returned object's speech can be modified if you want to add more information.
+
+    :return MyCityResponseDataModel: MyCityResponseDataModel with required fields
+        to request geolocation access
+    """
+    response = mycity_response_data_model.MyCityResponseDataModel()
+    response.output_speech = GENERIC_DEVICE_PERMISSON_SPEECH
+    response.card_type = "AskForPermissionsConsent"
+    response.card_permissions = ["read::alexa:device:all:address"]
+    response.should_end_session = True
+    return response
 
 def convert_mycity_coordinates_to_arcgis(mycity_request)->dict:
     """
