fb76252e688c95635bb5b837397f998a88290c39
Jamie Martini <martini.jamie@gmail.com>
2019-08-18 12:12:44 -0400

    In TrashDayIntent, handle if the user has given us device location permissions, but has not set a location for the device

--- mycity/mycity/utilities/location_services_utils.py
+++ mycity/mycity/utilities/location_services_utils.py
@@ -22,7 +22,8 @@ def get_address_from_user_device(mycity_request):
     in the session attributes
     :param mycity_request: MyCityRequestDataModel
     :param mycity_response: MyCityResponseDataModel
-    :return : MyCityRequestModel object
+    :return : MyCityRequestModel object and boolean indicating if we have
+        device address permissions
     """
     logger.debug('MyCityRequestDataModel received:' + mycity_request.get_logger_string())
 
@@ -38,7 +39,7 @@ def get_address_from_user_device(mycity_request):
         current_address = res['addressLine1']
         mycity_request.session_attributes[
             intent_constants.CURRENT_ADDRESS_KEY] = current_address
-    return mycity_request
+    return mycity_request, response_object.status_code == 200
     
 
 def request_geolocation_permission_response():
