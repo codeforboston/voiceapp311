a8e0d8399ed32d606789eb0847203c5301cf3109
Joshua Decosta <joshdecosta@gmail.com>
2019-08-20 20:48:13 -0400

    removes useless test

--- mycity/mycity/test/unit_tests/test_mycity_controller.py
+++ mycity/mycity/test/unit_tests/test_mycity_controller.py
@@ -68,22 +68,10 @@ class MyCityControllerUnitTestCase(base.BaseTestCase):
         self.controller.on_intent(self.request)
         mock_intent.assert_called_with(self.request)
 
-    @mock.patch('requests.get')
-    @mock.patch('mycity.utilities.location_services_utils.request_device_address_permission_response')
-    def test_intent_that_needs_address_without_address_in_session_attributes(
-            self,
-            mock_res,
-            mock_intent
-    ):
-        self.request.intent_name = "TrashDayIntent"
-        mock_request = self._mock_response(status=200, json_data=test_constants.ALEXA_DEVICE_ADDRESS)
-        mock_res.return_value = mock_request
-        result = self.controller.on_intent(self.request)
-        self.assertEqual(True, result)
-        # mock_intent.assert_called_with(self.request)
 
     def test_unknown_intent(self):
         self.request.intent_name = "MadeUpIntent"
         self.request.session_attributes[intent_constants.CURRENT_ADDRESS_KEY] = '46 Everdean St'
         with self.assertRaises(ValueError):
             self.controller.on_intent(self.request)
+
