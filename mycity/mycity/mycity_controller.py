5a1e0a15bff1a7a22e9540be137c6b416a9970f9
Joshua Decosta <joshdecosta@gmail.com>
2020-07-22 21:43:18 -0400

    cleaning up general files

--- mycity/mycity/mycity_controller.py
+++ mycity/mycity/mycity_controller.py
@@ -24,7 +24,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-LAUNCH_SPEECH = "Welcome to the Boston Info skill. You can ask for help " \
+LAUNCH_SPEECH = "Welcome to the Boston Info skill. You can ask for help" \
                 "at any time, and I'll let you know what information I can " \
                 "provide. How can I help you?"
 
@@ -212,6 +212,7 @@ def get_welcome_response(mycity_request):
     mycity_response.session_attributes = mycity_request.session_attributes
     mycity_response.card_title = "Welcome"
     mycity_response.output_speech = LAUNCH_SPEECH
+    mycity_response.output_speech_type = "PlainText"
 
     # If the user either does not reply to the welcome message or says
     # something that is not understood, they will be prompted again with
