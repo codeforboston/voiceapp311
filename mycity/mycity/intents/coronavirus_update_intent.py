71ddee5719a5ec36e82ab3872f689514fe53406f
Jamie Martini <martini.jamie@gmail.com>
2020-09-20 14:45:08 -0400

    Boston has removed the Coronavirus info from their homepage. Just return the detailed info

--- mycity/mycity/intents/coronavirus_update_intent.py
+++ mycity/mycity/intents/coronavirus_update_intent.py
@@ -14,7 +14,6 @@ NEWS_SOUND = "<audio src=\"soundbank://soundlibrary/musical/amzn_sfx_electronic_
 NO_UPDATE_ERROR = "I'm not able to find an update right now. Please try again later."
 
 WELCOME_MESSAGE = "<speak>Here are the latest updates:"
-HOMEPAGE_URL = "https://www.boston.gov"
 CORONAVIRUS_DETAIL_URL = "https://www.boston.gov/news/coronavirus-disease-covid-19-boston"
 
 
@@ -32,7 +31,7 @@ def get_coronovirus_update(mycity_request):
     mycity_response.should_end_session = True
     try:
         mycity_response.output_speech = _construct_output_speech(
-            _get_homepage_text(), _get_coronavirus_detail_text())
+            _get_coronavirus_detail_text())
         mycity_response.output_speech_type = 'SSML'
     except ParseError:
         mycity_response.output_speech = NO_UPDATE_ERROR
@@ -40,15 +39,14 @@ def get_coronovirus_update(mycity_request):
     return mycity_response
 
 
-def _construct_output_speech(homepage_text, coronavirus_page_text):
+def _construct_output_speech(coronavirus_page_text):
     """
     Constructs output speech for the coronvirus update
 
-    :param homepage_text: String containing text from the homepage update
     :param coronavirus_page_text: String containing text from the coronavirus detail webpage
     :return: String for output speech
     """
-    text = WELCOME_MESSAGE + homepage_text + NEWS_SOUND + coronavirus_page_text + str("</speak>")
+    text = WELCOME_MESSAGE + NEWS_SOUND + coronavirus_page_text + str("</speak>")
     return text
 
 def _get_html_parser(url):
@@ -62,24 +60,6 @@ def _get_html_parser(url):
     parser = BeautifulSoup(url, "html.parser")
     url.close()
     return parser
-    
-
-def _get_homepage_text():
-    """
-    Gets coronavirus update text from Boston.gov homepage
-
-    :return: String with coronavirus update text
-    """
-    parser = _get_html_parser(HOMEPAGE_URL)
-
-    try:
-        coronavirus_article = parser.find(
-            'article', about="/reopening-during-covid-19-boston")
-        text_div = coronavirus_article.find(
-            'div', class_='field-type-text-long')
-        return escape_characters(text_div.get_text())
-    except:
-        raise ParseError
 
 
 def _get_coronavirus_detail_text():
